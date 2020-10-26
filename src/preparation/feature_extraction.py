53b3130448ca4f1f8bbb1f328636e5bf145e4a2c
Samir Amir <dr.samir.amir@gmail.com>
2020-10-26 14:22:26 +0100

    updates

---
+++ src/preparation/feature_extraction.py
@@ -0,0 +1,13 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+
+
+def tfidf_extractor(corpus):
+    vectorizer = TfidfVectorizer()
+    tfidf_score = vectorizer.fit_transform(corpus)
+    # get tfidf vector for first document
+    first_document_vector = tfidf_score[0]
+    df = pd.DataFrame(first_document_vector.T.todense(), index=vectorizer.get_feature_names(),
+                      columns=["tfidf"])
+    df.sort_values(by=["tfidf"], ascending=False, inplace=True)
+    return df
---
+++ tests/preparation/test_feature_extraction.py
@@ -0,0 +1,14 @@
+from src.preparation.feature_extraction import *
+import unittest
+
+
+class FeatureExtractionTest(unittest.TestCase):
+    @unittest.skip("Test ignored (just for experiment)")
+    def test_tfidf_extractor(self):
+        corpus = ["the house had a tiny little mouse",
+                  "the cat saw the mouse",
+                  "the mouse ran away from the house",
+                  "the cat finally ate the mouse",
+                  "the end of the mouse story"
+                  ]
+        print(tfidf_extractor(corpus))
