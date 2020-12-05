3c83da42b62d7b54b45295d1f5bac4ec90f1f194
Samir Amir <dr.samir.amir@gmail.com>
2020-12-05 08:53:40 +0100

    add some classic machine learning approaches for text classification

--- tests/modeling/test_machine_learning_classifiers.py
+++ tests/modeling/test_machine_learning_classifiers.py
@@ -1,17 +1,18 @@
 import unittest
-from src.modeling.cnn_classifier import *
 from src.modeling.machine_learning_classifier import *
 from src.processing.newsgroups_data_acquisition import *
+from sklearn.model_selection import train_test_split
 
 
 class MLClassificationTest(unittest.TestCase):
-    #@unittest.skip("Test ignored (used only experiment)")
-    def test_ml_classifier(self):
+    def test_ml_classifiers(self):
         dataset_train, dataset_test = download_newsgroups_dataset()
-        ml_classifier = MachineLearningClassifier
-        ml_classifier.process_training(ml_classifier, dataset_train,dataset_test)
-
-
-
+        X_train, X_test = tfidf_transformer(dataset_train, dataset_test)
+        #X_train, X_test = count_vectorizer(dataset_train, dataset_test)
+        y_train = dataset_train.target
+        y_test = dataset_test.target
+        ml_classifier = MachineLearningClassifier(X_train, y_train, X_test, y_test)
+        ml_classifier.train_multinomialNB()
+        ml_classifier.train_linearSVC()
 
 
